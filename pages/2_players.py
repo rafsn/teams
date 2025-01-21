d50c3dfdaeb68dad5d19244ba8a9e4f150b00d9a
rafs <santos_rafa1973@hotmail.com>
2025-01-21 11:44:50 -0300

    players

--- pages/2_players.py
+++ pages/2_players.py
@@ -13,3 +13,17 @@ player_stats = df_data[df_data["Name"] == player].iloc[0]
 
 st.image(player_stats["Photo"])
 st.title(f"{player_stats['Name']}")
+
+st.markdown(f"**Clube:** {player_stats['Club']}")
+st.markdown(f"**Posição:** {player_stats['Position']}")
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.markdown(f"**Idade**: {player_stats['Age']}")
+col2.markdown(f"**Altura**: {player_stats['Height']}")
+col3.markdown(f"**Peso**: {player_stats['Weight'] * 0.4563:.2f}")
+
+st.divider()
+st.subheader(f"Overall {player_stats['Overall']}")
+st.progress(int(player_stats["Overall"]))
+
